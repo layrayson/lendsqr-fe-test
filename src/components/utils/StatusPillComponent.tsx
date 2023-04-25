b8dc5d4da4468d91759779880ed8fc3158bcf911
layrayson <olalereazeez11@gmail.com>
2023-04-25 07:14:41 +0100

    FEAT: create status pil component

---
+++ src/components/utils/StatusPillComponent.tsx
@@ -0,0 +1,51 @@
+
+import styles from '../../styles/components/utils/StatusPillComponent.module.scss'
+interface StatusPillComponentProps {
+    status: string,
+}
+
+const StatusPillComponent = ({ status }: StatusPillComponentProps) => {
+
+    switch (status) {
+        case 'inactive':
+            return (
+                <div className={styles.pillContainer + ' ' + styles.pillGray}>
+                    <div>
+                        {status}
+                    </div>
+                </div>
+            );
+        case 'pending':
+            return (
+                <div className={styles.pillContainer + ' ' + styles.pillYellow}>
+                    <div>
+                        {status}
+                    </div>
+                </div>
+            );
+        case 'blacklisted':
+            return (
+                <div className={styles.pillContainer + ' ' + styles.pillRed}>
+                    <div>
+                        {status}
+                    </div>
+                </div>
+            );
+        case 'active':
+            return (
+                <div className={styles.pillContainer + ' ' + styles.pillGreen}>
+                    <div>
+                        {status}
+                    </div>
+                </div>
+            );
+
+
+
+        default:
+            return <div></div>
+
+    }
+}
+
+export default StatusPillComponent;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,181 +1,198 @@
 import custom from '../../styles/shared/Custom.module.scss';
 import styles from '../../styles/pages/UsersPage.module.scss';
 import Image from 'next/image';
+import StatusPillComponent from '@/components/utils/StatusPillComponent';
 
 const UsersPage = () => {
-  return (
-    <div className={styles.pageWrapper}>
-      <div className={custom.pageHeader}>
-        <h5>Users</h5>
-      </div>
-      <div className={styles.statsFlex}>
-        <div className={styles.statsFlexItem}>
-          <div className={styles.statAvatar + ' ' + custom.bgLightPink}>
-            <Image
-              src={'/assets/icons/users_stat.svg'}
-              alt="users"
-              height={22}
-              width={22}
-            />
-          </div>
-          <div className={styles.statTitle}>USERS</div>
-          <div className={styles.statValue}>2,453</div>
-        </div>{' '}
-        <div className={styles.statsFlexItem}>
-          <div className={styles.statAvatar + ' ' + custom.bgLightBlue}>
-            <Image
-              src={'/assets/icons/active_users_stats.svg'}
-              alt="users"
-              height={22}
-              width={22}
-            />
-          </div>
-          <div className={styles.statTitle}>ACTIVE USERS</div>
-          <div className={styles.statValue}>2,453</div>
-        </div>{' '}
-        <div className={styles.statsFlexItem}>
-          <div className={styles.statAvatar + ' ' + custom.bgLightOrange}>
-            <Image
-              src={'/assets/icons/users_with_loans_stat.svg'}
-              alt="users"
-              height={22}
-              width={22}
-            />
-          </div>
-          <div className={styles.statTitle}>USERS WITH LOANS</div>
-          <div className={styles.statValue}>12,453</div>
-        </div>{' '}
-        <div className={styles.statsFlexItem}>
-          <div className={styles.statAvatar + ' ' + custom.bgLightRed}>
-            <Image
-              src={'/assets/icons/users_with_savings_stat.svg'}
-              alt="users"
-              height={22}
-              width={22}
-            />
-          </div>
-          <div className={styles.statTitle}>USERS WITH SAVINGS</div>
-          <div className={styles.statValue}>102,453</div>
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={custom.pageHeader}>
+                <h5>Users</h5>
+            </div>
+            <div className={styles.statsFlex}>
+                <div className={styles.statsFlexItem}>
+                    <div className={styles.statAvatar + ' ' + custom.bgLightPink}>
+                        <Image
+                            src={'/assets/icons/users_stat.svg'}
+                            alt="users"
+                            height={22}
+                            width={22}
+                        />
+                    </div>
+                    <div className={styles.statTitle}>USERS</div>
+                    <div className={styles.statValue}>2,453</div>
+                </div>{' '}
+                <div className={styles.statsFlexItem}>
+                    <div className={styles.statAvatar + ' ' + custom.bgLightBlue}>
+                        <Image
+                            src={'/assets/icons/active_users_stats.svg'}
+                            alt="users"
+                            height={22}
+                            width={22}
+                        />
+                    </div>
+                    <div className={styles.statTitle}>ACTIVE USERS</div>
+                    <div className={styles.statValue}>2,453</div>
+                </div>{' '}
+                <div className={styles.statsFlexItem}>
+                    <div className={styles.statAvatar + ' ' + custom.bgLightOrange}>
+                        <Image
+                            src={'/assets/icons/users_with_loans_stat.svg'}
+                            alt="users"
+                            height={22}
+                            width={22}
+                        />
+                    </div>
+                    <div className={styles.statTitle}>USERS WITH LOANS</div>
+                    <div className={styles.statValue}>12,453</div>
+                </div>{' '}
+                <div className={styles.statsFlexItem}>
+                    <div className={styles.statAvatar + ' ' + custom.bgLightRed}>
+                        <Image
+                            src={'/assets/icons/users_with_savings_stat.svg'}
+                            alt="users"
+                            height={22}
+                            width={22}
+                        />
+                    </div>
+                    <div className={styles.statTitle}>USERS WITH SAVINGS</div>
+                    <div className={styles.statValue}>102,453</div>
+                </div>
+            </div>
+            <div className={styles.tableContainer}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                ORGANIZATION{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th>
+                                USERNAME{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th>
+                                EMAIL{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th>
+                                PHONE NUMBER{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th>
+                                DATE JOINED{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th>
+                                STATUS{' '}
+                                <button className={custom.plainBtn}>
+                                    <Image
+                                        src={'/assets/icons/filter_icon.svg'}
+                                        height={10.67}
+                                        width={16}
+                                        alt="filter"
+                                    />
+                                </button>
+                            </th>
+                            <th><div ></div></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Lendsqr</td>
+                            <td>Adeddeji</td>
+                            <td>adedeji@lendsqr.com</td>
+                            <td>08097127881</td>
+                            <td>May 15, 2020 10:00 AM</td>
+                            <td><StatusPillComponent status='blacklisted' /></td>
+                            <td>
+                                <button className={custom.plainBtn}><i className='bx bx-dots-vertical-rounded'></i></button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Lendsqr</td>
+                            <td>Adeddeji</td>
+                            <td>adedeji@lendsqr.com</td>
+                            <td>08097127881</td>
+                            <td>May 15, 2020 10:00 AM</td>
+                            <td>s</td>
+                            <td>
+                                <button className={custom.plainBtn}><i className='bx bx-dots-vertical-rounded'></i></button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Lendsqr</td>
+                            <td>Adeddeji</td>
+                            <td>adedeji@lendsqr.com</td>
+                            <td>08097127881</td>
+                            <td>May 15, 2020 10:00 AM</td>
+                            <td>s</td>
+                            <td>
+                                <button className={custom.plainBtn}><i className='bx bx-dots-vertical-rounded'></i></button>
+                            </td>
+                        </tr>{' '}
+                        <tr>
+                            <td>Lendsqr</td>
+                            <td>Adeddeji</td>
+                            <td>adedeji@lendsqr.com</td>
+                            <td>08097127881</td>
+                            <td>May 15, 2020 10:00 AM</td>
+                            <td>s</td>
+                            <td>
+                                <button className={custom.plainBtn}><i className='bx bx-dots-vertical-rounded'></i></button>
+                            </td>
+                        </tr>{' '}
+                        <tr>
+                            <td>Lendsqr</td>
+                            <td>Adeddeji</td>
+                            <td>adedeji@lendsqr.com</td>
+                            <td>08097127881</td>
+                            <td>May 15, 2020 10:00 AM</td>
+                            <td>s</td>
+                            <td>
+                                <button className={custom.plainBtn}><i className='bx bx-dots-vertical-rounded'></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-      <div className={styles.tableContainer}>
-        <table>
-          <thead>
-            <tr>
-              <th>
-                ORGANIZATION{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-              <th>
-                USERNAME{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-              <th>
-                EMAIL{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-              <th>
-                PHONE NUMBER{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-              <th>
-                DATE JOINED{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-              <th>
-                STATUS{' '}
-                <button className={custom.plainBtn}>
-                  <Image
-                    src={'/assets/icons/filter_icon.svg'}
-                    height={10.67}
-                    width={16}
-                    alt="filter"
-                  />
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Lendsqr</td>
-              <td>Adeddeji</td>
-              <td>adedeji@lendsqr.com</td>
-              <td>08097127881</td>
-              <td>May 15, 2020 10:00 AM</td>
-              <td>s</td>
-            </tr>
-            <tr>
-              <td>Lendsqr</td>
-              <td>Adeddeji</td>
-              <td>adedeji@lendsqr.com</td>
-              <td>08097127881</td>
-              <td>May 15, 2020 10:00 AM</td>
-              <td>s</td>
-            </tr>
-            <tr>
-              <td>Lendsqr</td>
-              <td>Adeddeji</td>
-              <td>adedeji@lendsqr.com</td>
-              <td>08097127881</td>
-              <td>May 15, 2020 10:00 AM</td>
-              <td>s</td>
-            </tr>{' '}
-            <tr>
-              <td>Lendsqr</td>
-              <td>Adeddeji</td>
-              <td>adedeji@lendsqr.com</td>
-              <td>08097127881</td>
-              <td>May 15, 2020 10:00 AM</td>
-              <td>s</td>
-            </tr>{' '}
-            <tr>
-              <td>Lendsqr</td>
-              <td>Adeddeji</td>
-              <td>adedeji@lendsqr.com</td>
-              <td>08097127881</td>
-              <td>May 15, 2020 10:00 AM</td>
-              <td>s</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default UsersPage;
